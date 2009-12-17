4b00b5894e816c7e305cedfc3f08bfb95ba7bf9a
Julien Desrosiers <juliend2@mac-mini-de-julien-desrosiers-2.local>
2009-12-16 20:13:05 -0500

    ajouter les styles, et terminer laffichage des heures dans le tableau

--- index.py
+++ index.py
@@ -4,7 +4,7 @@
 import os
 import re
 import datetime
-from temps import TempsDate
+from temps import TempsDate,HourBloc
 
 path = '/Users/juliend2/Desktop/Dropbox/perso/ressources/TEMPS/'
 
@@ -15,6 +15,7 @@ print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.o
 print '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'
 print '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'
 print '<title>TemPy</title>'
+print '<link href="/static/styles.css" media="screen" rel="stylesheet" type="text/css" />' # symlinked from outside the cgi-bin
 print '</head><body>'
 
 timeslist= []
@@ -33,21 +34,31 @@ now = datetime.date.today()
 
 # print timeslist[0].date.day # the first day
 
-print '<table border="1"><tr>'
+print '<table border="0" width="100%" cellpadding="0" cellspacing="0"><tr>'
 
-for d in range(0, (now-timeslist[0].date).days):
+for d in range(0, (now-timeslist[0].date).days+1):
 	currentdate = timeslist[0].date + datetime.timedelta(days=d)
 	if currentdate.weekday() == 0:
 		print '</tr><tr>'
-	print '<td>'
-	print str(currentdate)
+	print '<td valign="top"><span class="weekday">'+currentdate.strftime("%A")+'</span>'
+	print '<h1>'+currentdate.strftime("%d %B %Y") + '</h1>'
 	currTemps = datetime.date(currentdate.year, currentdate.month, currentdate.day)
 	if currTemps in dateslist:
 		tdateobj = TempsDate(currentdate.strftime("%y"), currentdate.month, currentdate.day)
 		datefile = open(path + tdateobj.to_filename())
 		# lines = ''.join(datefile.readlines())
 		hourblocs = re.split('\n\t?\n', ''.join(datefile.readlines()))
-		
+		for bloc in hourblocs:
+			try:
+				hbloc = HourBloc(bloc)
+				print "<h2>"+hbloc.proj_name+":</h2>"
+				hours = hbloc.get_minutes()/60
+				mins = hbloc.get_minutes()%60
+				print "<p>"+str(hours)+'h'+ "%02d"%mins +"</p>"
+				
+			except AttributeError: # when we grab a line that is not a project bloc
+				pass
+			
 		
 	print '</td>'
 	
--- temps.py
+++ temps.py
@@ -10,6 +10,7 @@ Copyright (c) 2009 Julien Desrosiers. All rights reserved.
 import sys
 import os
 import datetime
+import re
 
 class TempsDate:
 	day=0
@@ -23,7 +24,52 @@ class TempsDate:
 	def to_filename(self):
 		return self.date.strftime("%y-%m-%d.txt")
 
+
+class HourBloc:
+	proj_name = ''
+	temps_desc = ''
+	proj_string = ''
+	def __init__(self, project_string):
+		self.proj_string = project_string
+		self.temps_desc = self._get_name()
+		self.proj_name = self._get_short_name()
+
+	def get_minutes(self):
+		minutes = 0
+		for t in self._get_hour_lines_list() :
+			# print int(t.group(1))
+			tfrom = datetime.datetime(2007,01,01,int(t.group(1)), int(t.group(2)))
+			tto = datetime.datetime(2007,01,01,int(t.group(3)), int(t.group(4)))
+			tdelta = (tto-tfrom)
+			minutes += tdelta.seconds / 60
+		return minutes
+
+	def _get_name(self):
+		match = re.match('([^:]+)', self.proj_string)
+		return match.group(0)
+
+	def _get_short_name(self):
+		match = re.match('([^\s]+)', self.proj_string)
+		return match.group(0)
+
+	def _get_hour_lines_list(self):
+		lines  = re.split('\n\t', self.proj_string)
+		tlist = []
+		for line in lines:
+			m = re.match('(\d?\d)h(\d\d) - (\d?\d)h(\d\d)', line) # get the hour-minutes
+			if m:
+				tlist.append(m) # return all the matches objects
+		return tlist		
+
+
+
 if __name__ == '__main__':
-	t = TempsDate(23,10,1985)
+	t = TempsDate(1985,10,23)
 	print t.to_filename() # should output: 85-10-23.txt
+	
+	h = HourBloc("coiteux (ajouter script seo) :\n\t9h25 - 9h45\n\t9h55 - 10h00")
+	print h._get_hour_lines_list()
+ 	print h.get_minutes()
+	print h.proj_name
+	print h.temps_desc
 
